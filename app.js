704c580c9446ae03a8141add6f5362ed3d00478c
BhagYagam <bhagyagmge@gmail.com>
2024-09-14 14:29:02 +0530

    first commit

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ fetch("https://api.weatherapi.com/v1/current.json?key=bec731a0d7e645759821239582
     dew1.innerText =data.current.dewpoint_c;
     console.log(data.current.dewpoint_c);
 
-    let img=document.getElementsByClassName("img");
-    img.innerText =data.current.condition.icon;
+    let img=document.getElementsByClassName("pic");
+    img.src =data.current.condition.icon;
     console.log(data.current.condition.icon);
    
 });
@@ -47,7 +47,7 @@ fetch("https://api.weatherapi.com/v1/current.json?key=bec731a0d7e645759821239582
 
 function serchCity(){
     let txtsearch=document.getElementById("txtsearch").value;
-
+    
     locationname = document.getElementById("locationname");
     country = document.getElementById("country");
     time =  document.getElementById("time");
@@ -57,6 +57,7 @@ function serchCity(){
     humidity1 = document.getElementById("humidity1");
     wind1 = document.getElementById("wind1");
     dew1 = document.getElementById("dew1");
+    img = document.getElementById("pic");
 
     console.log(txtsearch);
     fetch(`https://api.weatherapi.com/v1/current.json?key=bec731a0d7e64575982123958241009&q=${txtsearch}&aqi=no`)
@@ -74,6 +75,7 @@ function serchCity(){
         humidity1.innerHTML = data.current.humidity;
         wind1.innerHTML = data.current.wind_kph;
         dew1.innerHTML = data.current.dewpoint_c;
+        img.src = data.current.condition.icon;
            
     })
 }
@@ -81,41 +83,77 @@ function serchCity(){
 function forecast() {
     let txtsearch = document.getElementById("txtsearch").value
     fetch(`https://api.weatherapi.com/v1/forecast.json?key=bec731a0d7e64575982123958241009&q=${txtsearch}&days=9&aqi=no`)
+    // fetch(`https://api.weatherapi.com/v1/forecast.json?key=bec731a0d7e64575982123958241009&q=${txtsearch}&days=9&aqi=no`)
         .then(res => res.json())
         .then(data => {
-            document.getElementById("dateforecast1").innerHTML = data.forecast.forecastday[1].date
-            document.getElementById("iconforecast1").innerHTML = `<img src="${data.forecast.forecastday[1].day.condition.icon}" alt="" width="70px" height="70px">`
-            document.getElementById("tempforecast1").innerHTML = data.forecast.forecastday[1].day.mintemp_c + "-" + data.forecast.forecastday[1].day.maxtemp_c + "°C"
 
-            document.getElementById("dateforecast2").innerHTML = data.forecast.forecastday[2].date
-            document.getElementById("iconforecast2").innerHTML = `<img src="${data.forecast.forecastday[2].day.condition.icon}" alt="" width="70px" height="70px">`
-            document.getElementById("tempforecast2").innerHTML = data.forecast.forecastday[2].day.mintemp_c + "-" + data.forecast.forecastday[2].day.maxtemp_c + "°C"
+            console.log(data);
+            let fc1=document.getElementById("fc1");
+            fc1.innerText =data.forecast.forecastday.date;
+            console.log(data.forecast.forecastday.date);
 
-            document.getElementById("dateforecast3").innerHTML = data.forecast.forecastday[3].date
-            document.getElementById("iconforecast3").innerHTML = `<img src="${data.forecast.forecastday[3].day.condition.icon}" alt="" width="70px" height="70px">`
-            document.getElementById("tempforecast3").innerHTML = data.forecast.forecastday[3].day.mintemp_c + "-" + data.forecast.forecastday[3].day.maxtemp_c + "°C"
+            let fc2=document.getElementById("fc2");
+            fc2.innerText =data.forecast.forecastday.date;
+            console.log(data.forecast.forecastday.date);
 
-            document.getElementById("dateforecast4").innerHTML = data.forecast.forecastday[4].date
-            document.getElementById("iconforecast4").innerHTML = `<img src="${data.forecast.forecastday[4].day.condition.icon}" alt="" width="70px" height="70px">`
-            document.getElementById("tempforecast4").innerHTML = data.forecast.forecastday[4].day.mintemp_c + "-" + data.forecast.forecastday[4].day.maxtemp_c + "°C"
+            let fc3=document.getElementById("fc3");
+            fc3.innerText =data.forecast.forecastday.date;
+            console.log(data.forecast.forecastday.date);
 
-            document.getElementById("dateforecast5").innerHTML = data.forecast.forecastday[5].date
-            document.getElementById("iconforecast5").innerHTML = `<img src="${data.forecast.forecastday[5].day.condition.icon}" alt="" width="70px" height="70px">`
-            document.getElementById("tempforecast5").innerHTML = data.forecast.forecastday[5].day.mintemp_c + "-" + data.forecast.forecastday[5].day.maxtemp_c + "°C"
+            let fc4=document.getElementById("fc4");
+            fc4.innerText =data.forecast.forecastday.date;
+            console.log(data.forecast.forecastday.date);
 
-            document.getElementById("dateforecast6").innerHTML = data.forecast.forecastday[6].date
-            document.getElementById("iconforecast6").innerHTML = `<img src="${data.forecast.forecastday[6].day.condition.icon}" alt="" width="70px" height="70px">`
-            document.getElementById("tempforecast6").innerHTML = data.forecast.forecastday[6].day.mintemp_c + "-" + data.forecast.forecastday[6].day.maxtemp_c + "°C"
+            let fc5=document.getElementById("fc5");
+            fc5.innerText =data.forecast.forecastday.date;
+            console.log(data.forecast.forecastday.date);
 
-            document.getElementById("dateforecast7").innerHTML = data.forecast.forecastday[7].date
-            document.getElementById("iconforecast7").innerHTML = `<img src="${data.forecast.forecastday[7].day.condition.icon}" alt="" width="70px" height="70px">`
-            document.getElementById("tempforecast7").innerHTML = data.forecast.forecastday[7].day.mintemp_c + "-" + data.forecast.forecastday[7].day.maxtemp_c + "°C"
+            let fc6=document.getElementById("fc6");
+            fc6.innerText =data.forecast.forecastday.date;
+            console.log(data.forecast.forecastday.date);
 
-            document.getElementById("dateforecast8").innerHTML = data.forecast.forecastday[8].date
-            document.getElementById("iconforecast8").innerHTML = `<img src="${data.forecast.forecastday[8].day.condition.icon}" alt="" width="70px" height="70px">`
-            document.getElementById("tempforecast8").innerHTML = data.forecast.forecastday[8].day.mintemp_c + "-" + data.forecast.forecastday[8].day.maxtemp_c + "°C"
+            let fc7=document.getElementById("fc7");
+            fc7.innerText =data.forecast.forecastday.date;
+            console.log(data.forecast.forecastday.date);
+
+            let fc8=document.getElementById("fc8");
+            fc8.innerText =data.forecast.forecastday.date;
+            console.log(data.forecast.forecastday.date);
+
+
+
+            // document.getElementById("dateforecast1").innerHTML = data.forecast.forecastday[1].date
+            // document.getElementById("iconforecast1").innerHTML = `<img src="${data.forecast.forecastday[1].day.condition.icon}" alt="" width="70px" height="70px">`
+            // document.getElementById("tempforecast1").innerHTML = data.forecast.forecastday[1].day.mintemp_c + "-" + data.forecast.forecastday[1].day.maxtemp_c + "°C"
+
+            // document.getElementById("dateforecast2").innerHTML = data.forecast.forecastday[2].date
+            // document.getElementById("iconforecast2").innerHTML = `<img src="${data.forecast.forecastday[2].day.condition.icon}" alt="" width="70px" height="70px">`
+            // document.getElementById("tempforecast2").innerHTML = data.forecast.forecastday[2].day.mintemp_c + "-" + data.forecast.forecastday[2].day.maxtemp_c + "°C"
+
+            // document.getElementById("dateforecast3").innerHTML = data.forecast.forecastday[3].date
+            // document.getElementById("iconforecast3").innerHTML = `<img src="${data.forecast.forecastday[3].day.condition.icon}" alt="" width="70px" height="70px">`
+            // document.getElementById("tempforecast3").innerHTML = data.forecast.forecastday[3].day.mintemp_c + "-" + data.forecast.forecastday[3].day.maxtemp_c + "°C"
+
+            // document.getElementById("dateforecast4").innerHTML = data.forecast.forecastday[4].date
+            // document.getElementById("iconforecast4").innerHTML = `<img src="${data.forecast.forecastday[4].day.condition.icon}" alt="" width="70px" height="70px">`
+            // document.getElementById("tempforecast4").innerHTML = data.forecast.forecastday[4].day.mintemp_c + "-" + data.forecast.forecastday[4].day.maxtemp_c + "°C"
+
+            // document.getElementById("dateforecast5").innerHTML = data.forecast.forecastday[5].date
+            // document.getElementById("iconforecast5").innerHTML = `<img src="${data.forecast.forecastday[5].day.condition.icon}" alt="" width="70px" height="70px">`
+            // document.getElementById("tempforecast5").innerHTML = data.forecast.forecastday[5].day.mintemp_c + "-" + data.forecast.forecastday[5].day.maxtemp_c + "°C"
+
+            // document.getElementById("dateforecast6").innerHTML = data.forecast.forecastday[6].date
+            // document.getElementById("iconforecast6").innerHTML = `<img src="${data.forecast.forecastday[6].day.condition.icon}" alt="" width="70px" height="70px">`
+            // document.getElementById("tempforecast6").innerHTML = data.forecast.forecastday[6].day.mintemp_c + "-" + data.forecast.forecastday[6].day.maxtemp_c + "°C"
+
+            // document.getElementById("dateforecast7").innerHTML = data.forecast.forecastday[7].date
+            // document.getElementById("iconforecast7").innerHTML = `<img src="${data.forecast.forecastday[7].day.condition.icon}" alt="" width="70px" height="70px">`
+            // document.getElementById("tempforecast7").innerHTML = data.forecast.forecastday[7].day.mintemp_c + "-" + data.forecast.forecastday[7].day.maxtemp_c + "°C"
+
+            // document.getElementById("dateforecast8").innerHTML = data.forecast.forecastday[8].date
+            // document.getElementById("iconforecast8").innerHTML = `<img src="${data.forecast.forecastday[8].day.condition.icon}" alt="" width="70px" height="70px">`
+            // document.getElementById("tempforecast8").innerHTML = data.forecast.forecastday[8].day.mintemp_c + "-" + data.forecast.forecastday[8].day.maxtemp_c + "°C"
         })
-
 }
 
 
